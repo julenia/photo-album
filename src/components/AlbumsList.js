f0e0fdb5d8c227f8a3a566767464741ffad13a91
julenia <julia.jankowska@onet.eu>
2019-07-19 12:27:00 +0200

    Action creation - beginning

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export default function AlbumsList(props) {
+  return (<div>
+    <h1>All Albums</h1>
+
+    There are { props.albums.length } albums available.
+    <ul>
+    {props.albums.map(album => {
+      return <li key={album.id}>{album.title}</li>
+    })}
+    </ul>
+  </div>)
+}
